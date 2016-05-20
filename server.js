bb6b01c0d7263a9e7f15c2389415d4eea8928ff4
Chris Perez <chris.k.perez@gmail.com>
2016-05-20 11:11:35 -0700

    working on routing

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const http = require('http');
+const url = require('url');
 
-http.createServer((req, res)=>{
-  res.writeHead(200, { "Content-Type":"text/plain"});
-  res.write('Hello World');
-  res.end();
-}).listen(8888);
+function start(route, handle){
+  function onRequest(req, res){
+    var pathname = url.parse(req.url).pathname;
+    console.log('Request for ' + pathname + ' received.')
+
+    route(handle, pathname);
+
+    res.writeHead(200, { "Content-Type":"text/plain"});
+    res.write('Hello World');
+    res.end();
+  }
+
+  http.createServer(onRequest).listen(8888);
+  console.log('Server has started.')
+}
+
+exports.start = start;
