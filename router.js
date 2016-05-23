81347ddc62659b5956e24c120c90d02e2a5c21c3
Chris Perez <chris.k.perez@gmail.com>
2016-05-23 12:12:53 -0700

    project built

--- router.js
+++ router.js
@@ -1,9 +1,12 @@
-function route(pathname){
+function route(handle, pathname, res, req){
   console.log('About to route a request for ' + pathname);
   if (typeof handle[pathname] === 'function'){
-    handle[pathname]();
+    return handle[pathname](res, req);
   } else {
     console.log('No request found for ' + pathname);
+    res.writeHead(404, {'Content-Type': 'text/plain'});
+    res.write('404 Not Found');
+    res.end();
   }
 }
 
--- server.js
+++ server.js
@@ -4,14 +4,9 @@ const url = require('url');
 function start(route, handle){
   function onRequest(req, res){
     var pathname = url.parse(req.url).pathname;
-    console.log('Request for ' + pathname + ' received.')
-
-    route(handle, pathname);
-
-    res.writeHead(200, { "Content-Type":"text/plain"});
-    res.write('Hello World');
-    res.end();
-  }
+    console.log('Request for ' + pathname + ' received.');
+    route(handle, pathname, res, req);
+  };
 
   http.createServer(onRequest).listen(8888);
   console.log('Server has started.')
